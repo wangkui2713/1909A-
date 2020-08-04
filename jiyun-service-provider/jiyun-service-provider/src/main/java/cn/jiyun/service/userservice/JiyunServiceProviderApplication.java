30f5e4f9f7666a9c2c3abbde6e6cafe77cdc0e76
wangkui <1723175011@qq.com>
2020-08-04 16:35:16 +0800

    provice

---
+++ jiyun-service-provider/jiyun-service-provider/src/main/java/cn/jiyun/service/userservice/JiyunServiceProviderApplication.java
@@ -0,0 +1,14 @@
+package cn.jiyun.service.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiyunServiceProviderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JiyunServiceProviderApplication.class, args);
+    }
+
+}
